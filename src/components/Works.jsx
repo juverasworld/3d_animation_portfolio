32895ad43b27934be0b789509d8cd152074bc20f
Vee <ifebuchejulia@gmail.com>
2024-02-06 07:47:25 +0100

    added the live link

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -44,6 +44,16 @@ const ProjectCard = ({
                 className='w-1/2 h-1/2 object-contain'
               />
             </div>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
           </div>
         </div>
 
